06f510706e9667aa962adb2ed541c067ae00dbd9
mdamascena <msdamascena@gmail.com>
2023-02-15 17:21:13 -0300

    Alteração do Corssorigin

--- pages/saque-aniversario.jsx
+++ pages/saque-aniversario.jsx
@@ -22,9 +22,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Dongle:wght@300&family=Gochi+Hand&family=Poppins&family=Ubuntu:wght@300&display=swap" rel="stylesheet"></link>
-        <script src="https://kit.fontawesome.com/8d9284e27b.js" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/8d9284e27b.js" crossOrigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
       </Head>
 
