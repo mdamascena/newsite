0ea30a5ecfc539ca32505717eac038747e4c7137
mdamascena <msdamascena@gmail.com>
2023-06-20 22:38:01 -0300

    Criação da página Credluz

---
+++ components/CREDLUZ/MainCredluz.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import ImgMain from '../../public/img/pers_home.png'
+import styled from 'styled-components'
+import tw from 'tailwind-styled-components'
+import { useState, useEffect } from "react"
+
+export default function MainCredluz(){
+    return(
+        <main>
+            
+        </main>
+    )
+}
