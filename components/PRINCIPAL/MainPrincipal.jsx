bd59d335ef1575e44df87137a7e253913ebb54d5
mdamascena <msdamascena@gmail.com>
2023-07-26 13:31:05 -0300

    construção do BG na principal

--- components/PRINCIPAL/MainPrincipal.jsx
+++ components/PRINCIPAL/MainPrincipal.jsx
@@ -5,16 +5,16 @@ import imageTwo from "../../public/img/BRASAO_AZUL.png"
 
 export default function MainPrincipal() {
     return (
-        <main className="p-8 pt-32 md:px-16 lg:px-32 bgMainPrincipal">
+        <main className="p-8 pt-32 md:px-16 lg:px-32 bgMainPrincipal h-[100vh]">
             <section className="grid grid-cols-1 md:grid-cols-2 poppins">
 
-                <div className="my-16">
-                    <h1 className="text-[3.0rem] leading-[3.5rem] font-bold tracking-tight text-white"><span className="text-sky-400">Seu empréstimo ONLINE</span> com múltiplas opções de créditos</h1>
+                <div className="">
+                    <h1 className="text-[3.0rem] leading-[3.5rem] font-bold tracking-tight text-white"><span className="text-amber-500">Seu empréstimo ONLINE</span> com múltiplas opções de créditos</h1>
                     <h3 className="text-white mt-8 text-md">Mais autonomia, muito mais controle e economia de tempo para você. Feito para o seu perfil.</h3>
-                    <button className="bg-sky-700 rounded-2xl text-white py-3 px-16 mt-4 hover:bg-sky-800 duration-500">Simule aqui</button>
+                    <button className="bg-amber-500 rounded-2xl text-white py-3 px-16 mt-4 hover:bg-sky-800 duration-500">Simule aqui</button>
                 </div>
 
-                <div className="">
+                {/*<div className="">
                     <Carousel className=" "
                         autoPlay={'true'}
                         infiniteLoop={'true'}
@@ -35,7 +35,7 @@ export default function MainPrincipal() {
                             <Image width={50} height={50} src={imageTwo} alt="Imagem teste2" />
                         </div>
                     </Carousel>
-                </div>
+                </div>*/}
             </section>
         </main>
     )
