2950504f1ad46cd1a58319e33abce451a1b2d660
mdamascena <msdamascena@gmail.com>
2024-10-06 23:44:01 -0300

    slider home

--- components/PRINCIPAL/MODALIDADES/SlideModalidade.jsx
+++ components/PRINCIPAL/MODALIDADES/SlideModalidade.jsx
@@ -10,9 +10,9 @@ const LiMod = tw.li`
     my-6
     cursor-pointer
     duration-150
-    ${(p) => (p.$isActive ? 'text-slate-400' : 'text-slate-300 hover:text-slate-500')}
-    ${(p) => (p.$isActive ? 'lg:border-l-4 lg:border-blue-400' : 'lg:hover:border-l-4 lg:hover:border-blue-400')}
-`;
+    ${(p) => (p.$isActive ? 'text-blue-500' : 'text-slate-300 hover:text-blue-500')}
+    ${(p) => (p.$isActive ? 'lg:border-l-4 lg:border-blue-500' : 'lg:hover:border-l-4 lg:hover:border-blue-500')}
+`
 
 const Title = tw.h2`
     select-none
@@ -23,7 +23,7 @@ const Title = tw.h2`
     lg:text-left
     mb-3
     lg:mb-0
-`;
+`
 
 const Desc = tw.p`
     select-none
@@ -31,7 +31,7 @@ const Desc = tw.p`
     text-slate-300
     text-center
     lg:text-left
-`;
+`
 
 export default function ModSlide() {
     const [activeIndex, setActiveIndex] = useState(0);
@@ -39,7 +39,7 @@ export default function ModSlide() {
     useEffect(() => {
         const interval = setInterval(() => {
             setActiveIndex((prevIndex) => (prevIndex + 1) % 4);
-        }, 500000); // Troca de slide a cada 5 segundos
+        }, 5000); // Troca de slide a cada 5 segundos
 
         return () => clearInterval(interval);
     }, []);
@@ -50,6 +50,7 @@ export default function ModSlide() {
 
     return (
         <section className='lg:mx-32 mx-4'>
+            
             <div className='lg:mt-20 mt-10 lg:mx-52 mx-4'>
                 <h1 className='text-blue-950 saturate-150 lg:text-4xl text-2xl font-semibold tracking-tight text-center'>
                     Descubra uma variedade de opções de empréstimos,
@@ -57,79 +58,90 @@ export default function ModSlide() {
                 </h1>
             </div>
             
-            <div>
-                <div className='grid grid-cols-1 lg:grid-cols-2 lg:my-14 my-10'>
-                    <div className='col-span-1 mx-2 lg:mx-12'>
-                        <div className={`bg-[url(../public/img/modelo_credluz.png)] bg-cover bg-no-repeat group overflow-hidden relative rounded-2xl h-80 lg:h-full ${activeIndex === 0 ? 'block' : 'hidden'}`}>
-                            <div className='h-0 group-hover:h-28 transform duration-400 bg-black/30 inset-x-0 bottom-0 absolute backdrop-blur-md cursor-pointer'>
-                                
-                                
-                                <div className='grid grid-cols-5'>
-                                    <p className='text-white p-5 col-span-4'>Empréstimo débito na conta de luz</p>
-                                    <button className='text-2xl flex items-center justify-center bg-yellow-400 rounded-full w-10 h-10 text-center text-white col-span-1 m-5'>
-                                        <IoIosArrowForward/>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className={`bg-blue-500 rounded-2xl h-80 lg:h-full ${activeIndex === 1 ? 'block' : 'hidden'}`} />
-                        <div className={`bg-red-500 rounded-2xl h-80 lg:h-full ${activeIndex === 2 ? 'block' : 'hidden'}`} />
-                        <div className={`bg-green-500 rounded-2xl h-80 lg:h-full ${activeIndex === 3 ? 'block' : 'hidden'}`} />
-                    </div>
-
-                    <div className='col-span-1'>
-                        <ul>
-                            <LiMod $isActive={activeIndex === 0} onClick={() => setActiveIndex(0)} className={`lg:block ${activeIndex === 0 ? 'block' : 'hidden'}`}>
-                                <Title>Empréstimo da conta de luz</Title>
-                                <Desc>
-                                    Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
-                                    Antecipe seu saldo FGTS e realize o que quiser. S
-                                </Desc>
-                            </LiMod>
-                            
-                            <LiMod $isActive={activeIndex === 1} onClick={() => setActiveIndex(1)} className={`lg:block ${activeIndex === 1 ? 'block' : 'hidden'}`}>
-                                <Title>Antecipação saque FGTS</Title>
-                                <Desc>
-                                    Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
-                                    Antecipe seu saldo FGTS e realize o que qu
-                                </Desc>
-                            </LiMod>
-                            
-                            <LiMod $isActive={activeIndex === 2} onClick={() => setActiveIndex(2)} className={`lg:block ${activeIndex === 2 ? 'block' : 'hidden'}`}>
-                                <Title>Empréstimo consignado INSS</Title>
-                                <Desc>
-                                    Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
-                                    Antecipe seu saldo FGTS e real
-                                </Desc>
-                            </LiMod>
-                            
-                            <LiMod $isActive={activeIndex === 3} onClick={() => setActiveIndex(3)} className={`lg:block ${activeIndex === 3 ? 'block' : 'hidden'}`}>
-                                <Title>Empréstimo Pessoal</Title>
-                                <Desc>
-                                    Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
-                                    Antecipe seu saldo FGTS e rea
-                                </Desc>
-                            </LiMod>
-                        </ul>
-
-                        <div className='lg:hidden'>
-                            <div className='flex justify-center gap-2'>
-                                {[0, 1, 2, 3].map((index) => (
-                                    <motion.div
-                                        key={index}
-                                        className={`h-2 w-12 rounded-2xl ${activeIndex === index ? 'bg-blue-600' : 'bg-blue-300 hover:bg-blue-700'}`}
-                                        onClick={() => handleIndicatorClick(index)}
-                                        initial={{ opacity: 0.5 }}
-                                        animate={{ opacity: activeIndex === index ? 1 : 0.5 }}
-                                        transition={{ duration: 0.3 }}
-                                    />
-                                ))}
+            <div className='grid grid-cols-1 lg:grid-cols-2 lg:my-14 my-10'>
+                
+                <div className='col-span-1 mx-2 lg:mx-12'>
+                        
+                    <div className={`bg-[url(../public/img/modelo_credluz.png)] bg-cover bg-no-repeat group overflow-hidden relative rounded-2xl h-80 lg:h-full ${activeIndex === 0 ? 'block' : 'hidden'}`}>
+                        <div className='h-0 group-hover:h-28 transform duration-400 bg-black/30 inset-x-0 bottom-0 absolute backdrop-blur-md cursor-pointer'>
+                            <div className='grid grid-cols-5'>
+                                <p className='text-white p-5 col-span-4'>Empréstimo débito na conta de luz</p>
+                                <button className='text-2xl flex items-center justify-center bg-yellow-400 rounded-full w-10 h-10 text-center text-white col-span-1 m-5'>
+                                    <IoIosArrowForward/>
+                                </button>
                             </div>
                         </div>
                     </div>
+
+                    <div className={`bg-[url(../public/img/modelo_fgts.png)] bg-cover bg-no-repeat group overflow-hidden relative rounded-2xl h-80 lg:h-full ${activeIndex === 1 ? 'block' : 'hidden'}`}>
+                        <div className='h-0 group-hover:h-28 transform duration-400 bg-black/30 inset-x-0 bottom-0 absolute backdrop-blur-md cursor-pointer'>
+                            <div className='grid grid-cols-5'>
+                                <p className='text-white p-5 col-span-4'>Saque Antecipado do FGTS</p>
+                                <button className='text-2xl flex items-center justify-center bg-yellow-400 rounded-full w-10 h-10 text-center text-white col-span-1 m-5'>
+                                    <IoIosArrowForward/>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                        
+                    <div className={`bg-red-500 rounded-2xl h-80 lg:h-full ${activeIndex === 2 ? 'block' : 'hidden'}`} />
+                    <div className={`bg-green-500 rounded-2xl h-80 lg:h-full ${activeIndex === 3 ? 'block' : 'hidden'}`} />
+                
+                </div>
+
+                <div className='col-span-1 pr-16'>
+                    <ul>
+                        <LiMod $isActive={activeIndex === 0} onClick={() => setActiveIndex(0)} className={`lg:block ${activeIndex === 0 ? 'block' : 'hidden'}`}>
+                            <Title>Empréstimo da conta de luz</Title>
+                            <Desc>
+                                Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
+                                Antecipe seu saldo FGTS e realize o que quiser. S
+                            </Desc>
+                        </LiMod>
+                            
+                        <LiMod $isActive={activeIndex === 1} onClick={() => setActiveIndex(1)} className={`lg:block ${activeIndex === 1 ? 'block' : 'hidden'}`}>
+                            <Title>Antecipação saque FGTS</Title>
+                            <Desc>
+                                Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
+                                Antecipe seu saldo FGTS e realize o que qu
+                            </Desc>
+                        </LiMod>
+                            
+                        <LiMod $isActive={activeIndex === 2} onClick={() => setActiveIndex(2)} className={`lg:block ${activeIndex === 2 ? 'block' : 'hidden'}`}>
+                            <Title>Empréstimo consignado INSS</Title>
+                            <Desc>
+                                Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
+                                Antecipe seu saldo FGTS e real
+                            </Desc>
+                        </LiMod>
+                            
+                        <LiMod $isActive={activeIndex === 3} onClick={() => setActiveIndex(3)} className={`lg:block ${activeIndex === 3 ? 'block' : 'hidden'}`}>
+                            <Title>Empréstimo Pessoal</Title>
+                            <Desc>
+                                Antecipe seu saldo FGTS e realize o que quiser. Sem parcela mensal
+                                Antecipe seu saldo FGTS e rea
+                            </Desc>
+                        </LiMod>
+                    </ul>
+
+                    <div className='lg:hidden'>
+                        <div className='flex justify-center gap-2'>
+                            {[0, 1, 2, 3].map((index) => (
+                                <motion.div
+                                    key={index}
+                                    className={`h-2 w-12 rounded-2xl ${activeIndex === index ? 'bg-blue-600' : 'bg-blue-300 hover:bg-blue-700'}`}
+                                    onClick={() => handleIndicatorClick(index)}
+                                    initial={{ opacity: 0.5 }}
+                                    animate={{ opacity: activeIndex === index ? 1 : 0.5 }}
+                                    transition={{ duration: 0.3 }}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                
                 </div>
             </div>
+            
         </section>
     );
 }
