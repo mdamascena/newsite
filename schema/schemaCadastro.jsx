6757569ee051881c5f30384375fa1f478f6b2bbc
Thiago Bronisio <thiagobronisioo@gmail.com>
2024-10-07 23:54:27 -0300

    correção validações

--- schema/schemaCadastro.jsx
+++ schema/schemaCadastro.jsx
@@ -21,13 +21,15 @@ export const cadastroSchema = z.object({
 });
 
 export const identificacaoSchema = z.object({
-  nome: z.string().refine(value => validateFullName(value), { message: "Preencha o seu nome completo!" }),
-  nome: z.string().regex(/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/, { message: "O campo deve conter apenas letras." }),
-  nome: z.string().regex(/^[A-Za-z]+$/, { message: "O campo deve conter apenas letras." }),
+  nome: z.string()
+  .refine(value => validateFullName(value), { message: "Preencha o seu nome completo!" })
+  .refine(value => /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/.test(value), { message: "O campo deve conter apenas letras.." }),
   dataNascimento: z
     .string()
     .length(10, { message: "Digite uma data válida." })
     .refine((value) => {
+      if (!value) return true; // Não valida se o campo estiver vazio
+      
       // Parsing da data no formato dd/MM/yyyy
       const parsedDate = parse(value, "dd/MM/yyyy", new Date());
 
@@ -37,21 +39,19 @@ export const identificacaoSchema = z.object({
       }
 
       const today = new Date();
-
-      // Calcula a diferença exata em anos
       const ageInYears = differenceInYears(today, parsedDate);
 
       // Verifica se a idade está abaixo do limite de 21 anos
       if (ageInYears < MIN_AGE || (ageInYears === MIN_AGE && isBefore(today, parsedDate.setFullYear(parsedDate.getFullYear() + MIN_AGE)))) {
-        throw new z.ZodError([{ message: "Idade menor que 21 anos, não permitida", path: ["dataNascimento"] }]);
+        return false; // Retorna falso, não lança erro
       }
 
       // Verifica se a idade está acima do limite de 84 anos
       if (ageInYears > MAX_AGE || (ageInYears === MAX_AGE && !isBefore(today, parsedDate.setFullYear(parsedDate.getFullYear() + MAX_AGE)))) {
-        throw new z.ZodError([{ message: "Idade maior que 84 anos, não permitida", path: ["dataNascimento"] }]);
+        return false; // Retorna falso, não lança erro
       }
 
       return true;
-    }),
+    }, { message: "Idade deve estar entre 21 e 84 anos." }),
   genero: z.enum(["0", "1"], { errorMap: () => ({ message: "Selecione um gênero" }) }),
 });
