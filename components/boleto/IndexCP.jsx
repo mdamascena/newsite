4c132eb007299ff3dbc62dffc08e39ea46ab668a
Thiago Bronisio <thiagobronisioo@gmail.com>
2025-02-02 21:32:00 -0300

    comentando boleto

--- components/boleto/IndexCP.jsx
+++ components/boleto/IndexCP.jsx
@@ -1,14 +1,14 @@
-import Footer from "../geral/footer/Footer"
+// import Footer from "../geral/footer/Footer"
 
-import Header from "../geral/header/Header"
+// import Header from "../geral/header/Header"
 
 
-export default function IndexCP() {
-    return (
-        <>
-            <Header />
+// export default function IndexCP() {
+//     return (
+//         <>
+//             <Header />
             
-            <Footer />
-        </>
-    )
-}
+//             <Footer />
+//         </>
+//     )
+// }
