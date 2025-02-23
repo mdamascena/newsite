671e37604dcdf4fc72d152c04afe85ea7d8a5d0d
Thiago Bronisio <thiagobronisioo@gmail.com>
2025-02-23 12:11:24 -0300

    feat: att lib e correções

--- components/fgts/form/FormPagamento.jsx
+++ components/fgts/form/FormPagamento.jsx
@@ -7,7 +7,6 @@ import { BsCreditCard2Front } from "react-icons/bs";
 import { IoAt } from "react-icons/io5";
 import { HiOutlineDevicePhoneMobile } from "react-icons/hi2"
 import { Select, SelectTrigger, SelectValue, SelectItem, SelectGroup, SelectLabel, SelectContent } from "../../ui/selectFC"
-import { useEffect } from "react";
 import { ToastContainer } from "react-toastify"
 import { toastWarningColored } from "shared/toastUtils/toastValidation";
 import { motion } from "framer-motion";
@@ -15,10 +14,11 @@ import { container, item } from "shared/motionUtils/motionTransation";
 import { OptLabel } from "components/geral/style";
 import BtnNext from "../../geral/button/BtnBlueNext";
 import BtnBack from "../../geral/button/BtnBlueBack";
+import { useEffect } from "react";
 
 export default function FormPagamento({ onNext, backStep }) {
     const { register, control, handleSubmit, setValue, watch, getValues, formState: { errors } } = useFormContext();
-    const { atualizarForm } = useFormData();
+    const { atualizarForm, formData } = useFormData();
     const registerWithMask = useHookFormMask(register)
 
     const selectedKey = watch("tipoDeChave");
@@ -52,6 +52,12 @@ export default function FormPagamento({ onNext, backStep }) {
         }
     };
 
+    useEffect(() => {
+        if(formData.banco){
+            setValue("banco", formData.banco)
+        }
+    }, [formData.banco])
+
     return (
         <form className="lg:min-h-[100vh] lg:overflow-y-hidden" onSubmit={handleSubmit(onSubmit)}>
             <ToastContainer />
@@ -75,12 +81,12 @@ export default function FormPagamento({ onNext, backStep }) {
 
                 <div  className="container-form-body">
                     <div className="col-span-6 mt-5">
-                        <Select className='placeholder:text-slate-400'  {...register('banco')} onValueChange={(value) => setValue('banco', value)} >
-                            <SelectTrigger className="py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500">
+                        <Select className='placeholder:text-slate-400' defaultValue={formData.banco}  {...register('banco')} onValueChange={(value) => setValue('banco', value)} >
+                            <SelectTrigger className="py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 border-slate-300">
                                 <SelectValue placeholder="Informe seu Banco" />
                             </SelectTrigger>
                                             
-                            <SelectContent >
+                            <SelectContent>
                                 <SelectGroup className="text-slate-400">
                                     <SelectLabel>Selecione o Banco</SelectLabel>
                                     <SelectItem value="0">237 - Banco Bradesco</SelectItem>
@@ -161,7 +167,7 @@ export default function FormPagamento({ onNext, backStep }) {
                                     {selectedKey === "CPF" && (
                                         <motion.div variants={container} className="lg:col-span-6 col-span-1 mt-4">
                                             <Input
-                                                className={`py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 ${
+                                                className={`py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 border-slate-300 ${
                                                     errors.chaveCpf ? "border-red-500 focus-visible:ring-red-500 placeholder:text-red-500 bg-red-50" : ""
                                                 }`}
                                                 type="text"
@@ -176,7 +182,7 @@ export default function FormPagamento({ onNext, backStep }) {
                                     {selectedKey === "Celular" && (
                                         <motion.div variants={container} className="lg:col-span-6 col-span-1 mt-4">
                                             <Input
-                                                className={`py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 ${
+                                                className={`py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 border-slate-300 ${
                                                     errors.chaveCel ? "border-red-500 focus-visible:ring-red-500 placeholder:text-red-500 bg-red-50" : ""
                                                 }`}
                                                 type="text"
@@ -190,7 +196,7 @@ export default function FormPagamento({ onNext, backStep }) {
                                     {selectedKey === "Email" && (
                                         <motion.div variants={container} className="lg:col-span-6 col-span-1 mt-4">
                                             <Input
-                                                className={`py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 ${
+                                                className={`py-6 bg-white placeholder:text-slate-400 focus-visible:ring-blue-500 border-slate-300 ${
                                                     errors.chaveEmail ? "border-red-500 focus-visible:ring-red-500 placeholder:text-red-500 bg-red-50" : ""
                                                 }`}
                                                 type="text"
