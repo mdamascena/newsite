7989321ba9a6575e37962ba5483b914f4d8efb1d
mdamascena <msdamascena@gmail.com>
2026-04-21 16:47:10 -0300

    chore: upgrade nextjs tailwind and toolchain

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+
+const config = [
+  ...nextCoreWebVitals,
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    rules: {
+      "react-hooks/incompatible-library": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/static-components": "off",
+    },
+  },
+];
+
+export default config;
--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-module.exports = {
-    reactStrictMode: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    webpack: (config) => {
-    config.module.rules.push({
-        test: /\.(mov|mp4)$/,
-        use: {
-            loader: 'file-loader',
-            options: {
-                publicPath: '/_next/static/', // Define o caminho onde os arquivos serão servidos
-                outputPath: 'static/', // Define o caminho de saída dos arquivos no diretório público
-            },
-        },
-    });
-
-    return config;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
